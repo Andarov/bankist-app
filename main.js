bb2826310bed413c4db3f0ed52fdc9eaf0caf2e5
Andarov <bekturandarob678@gmail.com>
2022-10-30 11:41:49 +0500

    Change objects to one put one array

--- main.js
+++ main.js
@@ -1,30 +1,31 @@
 'use strict';
 // Data
-const account1 = {
-  owner: 'Jonas Schmedtmann',
-  movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
-  interestRate: 1.2, // %
-  pin: 1111,
-};
-const account2 = {
-  owner: 'Jessica Davis',
-  movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
-  interestRate: 1.5,
-  pin: 2222,
-};
-const account3 = {
-  owner: 'Steven Thomas Williams',
-  movements: [200, -200, 340, -300, -20, 50, 400, -460],
-  interestRate: 0.7,
-  pin: 3333,
-};
-const account4 = {
-  owner: 'Sarah Smith',
-  movements: [430, 1000, 700, 50, 90],
-  interestRate: 1,
-  pin: 4444,
-};
-const accounts = [account1, account2, account3, account4];
+const accounts = [
+  {
+    owner: 'Jonas Schmedtmann',
+    movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
+    interestRate: 1.2,
+    pin: 1111,
+  },
+  {
+    owner: 'Jessica Davis',
+    movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
+    interestRate: 1.5,
+    pin: 2222,
+  },
+  {
+    owner: 'Steven Thomas Williams',
+    movements: [200, -200, 340, -300, -20, 50, 400, -460],
+    interestRate: 0.7,
+    pin: 3333,
+  },
+  {
+    owner: 'Sarah Smith',
+    movements: [430, 1000, 700, 50, 90],
+    interestRate: 1,
+    pin: 4444,
+  },
+]
 
 // Elements
 const labelWelcome = document.querySelector('.welcome');
@@ -60,16 +61,16 @@ const currencies = new Map([
 
 // Displayni yangilash
 const updateUI = function(account){
-    displayMov(account.movements);
-    displayBalance(account);
-    displaySummary(account)
-  }
+  displayMov(account.movements);
+  displayBalance(account);
+  displaySummary(account)
+}
 
 // User yaratish
 const createUser = function(accs){
-    accs.forEach(function(acc){
-      acc.user = acc.owner.toLowerCase().split(' ').map(name => name[0]).join('');
-    })
+  accs.forEach(function(acc){
+    acc.user = acc.owner.toLowerCase().split(' ').map(name => name[0]).join('');
+  })
 }
 createUser(accounts);
 
@@ -88,34 +89,34 @@ btnLogin.addEventListener('click', function(e){
 
 // Movementlarni displayga chiqarish
 const displayMov = function(movements, sort = false){
-    containerMovements.innerHTML = '';
-    const movs = sort ? movements.slice().sort((a, b)=> b-a) : movements
-    movs.forEach(function(mov, i){
-      const type = mov > 0 ? 'kirim' : 'chiqim';
-      const html = `
-      <div class="movements__row">
-        <div class="movements__type movements__type--${type}">${i + 1} ${type}</div>
-        <div class="movements__value">${mov}€</div>
-      </div>
-      `
-      containerMovements.insertAdjacentHTML('afterbegin', html)
-    })
+  containerMovements.innerHTML = '';
+  const movs = sort ? movements.slice().sort((a, b)=> b-a) : movements
+  movs.forEach(function(mov, i){
+    const type = mov > 0 ? 'kirim' : 'chiqim';
+    const html = `
+    <div class="movements__row">
+    <div class="movements__type movements__type--${type}">${i + 1} ${type}</div>
+    <div class="movements__value">${mov}€</div>
+    </div>
+    `
+    containerMovements.insertAdjacentHTML('afterbegin', html)
+  })
 }
 
 // Umumiy balansni chiqarish va uni displayga chiqarish
 const displayBalance = function(account) {
-    account.balance = account.movements.reduce((acc, cur) => acc + cur, 0)
-    labelBalance.innerHTML = `${account.balance}€`;
+  account.balance = account.movements.reduce((acc, cur) => acc + cur, 0)
+  labelBalance.innerHTML = `${account.balance}€`;
 }
 
 // Outcome income va interestlarmni hisoblash
 const displaySummary = function(account){
-    const incomes = account.movements.filter(mov => mov > 0).reduce((acc, mov) => acc + mov)
-    labelSumIn.innerHTML = `${incomes}€`
-    const outcomes = account.movements.filter(mov => mov < 0).reduce((acc, mov) => acc + mov)
-    labelSumOut.innerHTML = `${Math.abs(outcomes)}€`
-    const interst = account.movements.filter(mov => mov > 0).map(deposit => (deposit * account.interestRate)/100).reduce((acc, mov) => acc + mov)
-    labelSumInterest.innerHTML = `${Math.floor(interst)}€`
+  const incomes = account.movements.filter(mov => mov > 0).reduce((acc, mov) => acc + mov)
+  labelSumIn.innerHTML = `${incomes}€`
+  const outcomes = account.movements.filter(mov => mov < 0).reduce((acc, mov) => acc + mov)
+  labelSumOut.innerHTML = `${Math.abs(outcomes)}€`
+  const interst = account.movements.filter(mov => mov > 0).map(deposit => (deposit * account.interestRate)/100).reduce((acc, mov) => acc + mov)
+  labelSumInterest.innerHTML = `${Math.floor(interst)}€`
 }
 
 // Sortlash
@@ -128,40 +129,40 @@ btnSort.addEventListener('click', function(e){
 
 // Transfer qilish
 btnTransfer.addEventListener('click', function(e){
-    e.preventDefault();
-    const reciever = accounts.find(acc => acc.user === inputTransferTo.value)
-    const amount = Number(inputTransferAmount.value)
-    if(amount > 0 && currentAcc.balance >= amount && reciever.user !== currentAcc.user) {
-      currentAcc.movements.push(-amount)
-      currentAcc.balance - amount
-      reciever.movements.push(amount)
-      reciever.balance + amount
-      updateUI(currentAcc)
-      inputTransferTo.value = inputTransferAmount.value = ''
-    }
+  e.preventDefault();
+  const reciever = accounts.find(acc => acc.user === inputTransferTo.value)
+  const amount = Number(inputTransferAmount.value)
+  if(amount > 0 && currentAcc.balance >= amount && reciever.user !== currentAcc.user) {
+    currentAcc.movements.push(-amount)
+    currentAcc.balance - amount
+    reciever.movements.push(amount)
+    reciever.balance + amount
+    updateUI(currentAcc)
+    inputTransferTo.value = inputTransferAmount.value = ''
+  }
 })
 
 // Kredit olish
 btnLoan.addEventListener('click', function(e){
-    e.preventDefault();
-    const loan = Number(Math.floor(inputLoanAmount.value));
-    if(loan > 0 && currentAcc.movements.some(mov => mov >= loan * 0.1)){
-      currentAcc.movements.push(loan);
-      inputLoanAmount.value = ''
-      updateUI(currentAcc)
-    }
+  e.preventDefault();
+  const loan = Number(Math.floor(inputLoanAmount.value));
+  if(loan > 0 && currentAcc.movements.some(mov => mov >= loan * 0.1)){
+    currentAcc.movements.push(loan);
+    inputLoanAmount.value = ''
+    updateUI(currentAcc)
+  }
 })
 
 // Tizimdan chiqish
 btnClose.addEventListener('click', function(e){
-    e.preventDefault();
-    if(currentAcc.pin === Number(inputClosePin.value) && currentAcc.user === inputCloseUsername.value){
-      const delAcc = accounts.findIndex(acc => acc.user === currentAcc.user)
-      accounts.splice(delAcc, 1)
-      containerApp.style.opacity = 0
-      inputClosePin.value = inputCloseUsername.value = ''
-      labelWelcome.textContent = 'Log in to get started'
-    }
+  e.preventDefault();
+  if(currentAcc.pin === Number(inputClosePin.value) && currentAcc.user === inputCloseUsername.value){
+    const delAcc = accounts.findIndex(acc => acc.user === currentAcc.user)
+    accounts.splice(delAcc, 1)
+    containerApp.style.opacity = 0
+    inputClosePin.value = inputCloseUsername.value = ''
+    labelWelcome.textContent = 'Log in to get started'
+  }
 })  
 
 
